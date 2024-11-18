93dee9b88efb8da1fea494476d51f13e40d3634a
Tomako-sys <121187174+Tomako-sys@users.noreply.github.com>
2024-11-18 17:29:42 +0000

    update Index.tsx:bug

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,13 +5,30 @@ import { Trophy, Flame, Star } from "lucide-react";
 import { Link } from "react-router-dom";
 import HamburgerMenu from "@/components/HamburgerMenu";
 
-  const Index = () => {
-    return (
-      <div>
-        <HamburgerMenu />
-        <h1>Bienvenue sur la page d'accueil</h1>
+const Index = () => {
+  const { t } = useLanguage();
+
+  return (
+    <div>
+      <HamburgerMenu />
+      <h1>{t("welcome")}</h1>
+      <ProgressBar />
+      <div className="flex justify-around mt-4">
+        <Link to="/lessons" className="flex flex-col items-center">
+          <Trophy className="h-8 w-8 text-primary" />
+          <span>{t("nav.lessons")}</span>
+        </Link>
+        <Link to="/practice" className="flex flex-col items-center">
+          <Flame className="h-8 w-8 text-primary" />
+          <span>{t("nav.practice")}</span>
+        </Link>
+        <Link to="/progress" className="flex flex-col items-center">
+          <Star className="h-8 w-8 text-primary" />
+          <span>{t("nav.progress")}</span>
+        </Link>
       </div>
-    );
-  };
-  
-  export default Index;
+    </div>
+  );
+};
+
+export default Index;
